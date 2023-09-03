654489814ba4b3dfd065652b2cbd9bb016f6f15c
Iv Karavaiev <ddev4620@gmail.com>
2023-09-03 21:09:39 +0300

    Part 1.13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,22 +1,33 @@
 import { useState } from 'react'
+const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+]
+let anecdotesCount = anecdotes.length
+let points = new Array(anecdotesCount).fill(0);
 
 const App = () => {
-    const anecdotes = [
-        'If it hurts, do it more often.',
-        'Adding manpower to a late software project makes it later!',
-        'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-        'Premature optimization is the root of all evil.',
-        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-        'The only way to go fast, is to go well.'
-    ]
 
     const [selected, setSelected] = useState(0)
     const handleRandomAnecdot = () => {
-        let anecdotesCount = anecdotes.length
         let randomAnecdot = Math.floor(Math.random() * anecdotesCount);
         setSelected(randomAnecdot)
+    }
+    const handleVoteAnecdot = (selected) => {
+        return (
+            () => {
+                const copy = [...points]
+                copy[selected] += 1
+                points = copy
+            }
+        )
+
     }
 
     return (
@@ -24,6 +35,7 @@ const App = () => {
             <div>
                 {anecdotes[selected]}
             </div>
+            <button onClick={handleVoteAnecdot(selected)}>vote</button>
             <button onClick={handleRandomAnecdot}>next anecdote</button>
         </div>
     )
