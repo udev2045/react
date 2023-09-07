fadc71ac95bd8ca9721108224a97cbb1315ce613
Iv Karavaiev <ddev4620@gmail.com>
2023-09-08 00:25:49 +0300

    Part 2.20

--- part2/countries/src/Components/SingleCountry/SingleCountry.jsx
+++ part2/countries/src/Components/SingleCountry/SingleCountry.jsx
@@ -1,8 +1,22 @@
 /* SingleCountry.jsx was created 07.09.2023 22:27 */
 import css from "./SingleCountry.module.css"
+import { useState, useEffect } from 'react'
+import axios from "axios";
 
 const SingleCountry = ({country}) => {
     const languages = Object.values(country.languages)
+    const lat = country.capitalInfo.latlng[0]
+    const lon = country.capitalInfo.latlng[1]
+    const weatherApiKey= '77334c9808d0f08ba650c647d54b2354'
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${weatherApiKey}`
+    console.log(weatherUrl)
+    const [weather, setWeather] = useState({})
+    useEffect(() => {
+        axios.get(weatherUrl).then(response => {
+            setWeather(response.data)
+        })
+    }, [])
+    const weatherIcon = Object.keys(weather).length === 0 ? '' :  `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
     return (
         <div>
             <h1>{country.name.common}</h1>
@@ -13,6 +27,14 @@ const SingleCountry = ({country}) => {
                 { languages.map( lang => <li>{lang}</li> ) }
             </ul>
             <img src={country.flags.svg} alt={country.flags.alt} width="100"/>
+            {Object.keys(weather).length === 0 ? '' :
+                <div>
+                <h2>Weather in {country.capital[0]}</h2>
+                <p>Temperature: {weather.main.temp}&deg;C</p>
+                <img src={weatherIcon}/>
+                <p>Wind: {weather.wind.speed}m/s</p>
+                </div>
+            }
         </div>
     )
 }
